ad988e6f6c306bfd9daf5fcc506ce7301d38261c
FettHsiao <116969114+FettHsiao@users.noreply.github.com>
2024-06-17 00:54:01 +0800

    Update EasyLigalization.cpp

--- EasyLigalization.cpp
+++ EasyLigalization.cpp
@@ -36,13 +36,22 @@ void ligalization(vector<Box>& points, vector<Box>& placement, double StartPoint
             double next = placement[0].x + PlaceWidth;
             double dis = placement[0].x + points[0].width;
             while(int(next/dis) != 0) {
-                if(int(next/dis) == 0):
+                if(int(next/dis) == 0)
                     break;
                 next += PlaceWidth;
             } 
             placement[0].x_new = next;
         } else {
-
+            placement[i].x = placement[i-1].x_new;
+            placement[i].y = points[i].y;
+            double next = placement[i].x + PlaceWidth;
+            double dis = placement[i].x + points[i].width;
+            while(int(next/dis) != 0) {
+                if(int(next/dis) == 0)
+                    break;
+                next += PlaceWidth;
+            } 
+            placement[i].x_new = next;
         }
         
     }
